139a3edb9e08c53971c130f5350dbb34ecb11fc6
exosuper <t.sdolui321@gmail.com>
2026-04-27 12:32:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 
+from http.server import SimpleHTTPRequestHandler
+import socketserver
+import os
+
+PORT = int(os.environ.get("PORT", 10000))
+
+Handler = SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
